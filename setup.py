331cf5118a7792d58ebd3f4229cce6e181d21013
CrossDarkrix <crosseddarkrix@gmail.com>
2025-07-01 06:28:18 +0900

    11

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='cheroot',
+    packages=['cheroot'],
+    version='1.1.9',
+    license='MIT',
+    description='test',
+    author='test',
+    author_email='test@test',
+    url='https://github.com/CrossDarkrix/test-cheroot',
+    keywords=['cheroot'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+)
